71ca215b3da21b3ea0358c569b0d52cc1dd01e43
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-11-04 14:11:46 +0100

    add some configurations to the ServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,54 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
+        $this->configureModels();
+        $this->configureFactoryImage();
+        $this->configureCommands();
+    }
 
+
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::unguard();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    private function configureFactoryImage()
+    {
+        Factory::macro('randImage', function (int $width, int $height): string {
+            return sprintf(
+                'https://picsum.photos/%d/%d',
+                $width,
+                $height
+            );
+        });
     }
 }
